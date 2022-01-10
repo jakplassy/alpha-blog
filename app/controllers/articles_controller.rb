117fa0fad14b40505a5b1ffa2e18fb440ed498f4
jakplassy <jakplassy@gmail.com>
2022-01-10 14:09:10 -0600

    created articles controller to create new ones

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,12 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def new
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+
+    @article.save
+  end 
 end
